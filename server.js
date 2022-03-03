20f9f862ae2275a3f5eb00d64fa69b8d916112a3
Harsh Patel <harshpatel4905@gmail.com>
2022-03-03 20:50:14 +0530

    for deploy again

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+
 const path = require('path');
+
 const app = express();
+
 app.use(express.static(__dirname + '/dist/landing-page'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/landing-page/index.html'));});
+
+
+//PATH LOCATION STARTEGY
+
+app.get('/*', function (req, res) {
+const fullPath = path.join(__dirname + '/dist/landing-page/');
+res.sendFile(fullPath);
+})
+
+
 app.listen(process.env.PORT || 8080);
